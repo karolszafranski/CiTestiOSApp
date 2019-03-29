50077287873809cc8bee6e71004e1d90a928efa2
Karol Szafrański <kszafranski@eclipsesource.com>
2019-03-29 14:19:57 +0100

    Initial Commit

---
+++ CiTestiOSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CiTestiOSApp
+//
+//  Created by Karol Szafrański on 29.03.19.
+//  Copyright © 2019 Karol Szafrański. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CiTestiOSApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CiTestiOSApp
+//
+//  Created by Karol Szafrański on 29.03.19.
+//  Copyright © 2019 Karol Szafrański. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
